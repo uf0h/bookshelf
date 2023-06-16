71c4915ce5873ed619362eaacfd9f1d5cb73205a
uf0h <3z7qwnsqth@gmail.com>
2023-06-16 17:00:22 +0400

    update libs

--- test/integration/helpers/config.js
+++ test/integration/helpers/config.js
@@ -2,12 +2,14 @@ module.exports = {
   mysql: {
     database: 'bookshelf_test',
     user: 'root',
+    password: 'password',
     encoding: 'utf8'
   },
 
   postgres: {
     database: 'bookshelf_test',
-    user: 'postgres'
+    user: 'postgres',
+    password: 'postgres'
   },
 
   sqlite3: {
